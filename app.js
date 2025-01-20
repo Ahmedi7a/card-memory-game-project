30681af624603838bb7b8ba7bbdb9e964889cb22
Ahmedi7a <ahmedamralla73@email.com>
2025-01-20 21:06:47 +0300

    fixed the win and lose clicks

--- app.js
+++ app.js
@@ -1,46 +1,51 @@
-const cardsArray = [
-    { alt: "watermelon", imageUrl: "./images/fruit_1.jpg" },
-    { alt: "watermelon", imageUrl: "./images/fruit_1.jpg" },
-    { alt: "fruit2", imageUrl: "./images/fruit_2.jpg" },
-    { alt: "fruit2", imageUrl: "./images/fruit_2.jpg" },
-    { alt: "fruit3", imageUrl: "./images/fruit_3.jpg" },
-    { alt: "fruit3", imageUrl: "./images/fruit_3.jpg" },
-    { alt: "fruit4", imageUrl: "./images/fruit_4.jpg" },
-    { alt: "fruit4", imageUrl: "./images/fruit_4.jpg" },
-    { alt: "fruit5", imageUrl: "./images/fruit_5.jpg" },
-    { alt: "fruit5", imageUrl: "./images/fruit_5.jpg" },
-    { alt: "fruit6", imageUrl: "./images/fruit_6.jpg" },
-    { alt: "fruit6", imageUrl: "./images/fruit_6.jpg" },
-];
+// const cardsArray = [
+//     { alt: "watermelon", imageUrl: "./images/fruit_1.jpg" },
+//     { alt: "watermelon", imageUrl: "./images/fruit_1.jpg" },
+//     { alt: "fruit2", imageUrl: "./images/fruit_2.jpg" },
+//     { alt: "fruit2", imageUrl: "./images/fruit_2.jpg" },
+//     { alt: "fruit3", imageUrl: "./images/fruit_3.jpg" },
+//     { alt: "fruit3", imageUrl: "./images/fruit_3.jpg" },
+//     { alt: "fruit4", imageUrl: "./images/fruit_4.jpg" },
+//     { alt: "fruit4", imageUrl: "./images/fruit_4.jpg" },
+//     { alt: "fruit5", imageUrl: "./images/fruit_5.jpg" },
+//     { alt: "fruit5", imageUrl: "./images/fruit_5.jpg" },
+//     { alt: "fruit6", imageUrl: "./images/fruit_6.jpg" },
+//     { alt: "fruit6", imageUrl: "./images/fruit_6.jpg" },
+// ];
 
 
 // func alt, url
 
 /*-------------------------------- Constants --------------------------------*/
 let firstCard, secondCard;
-let blockBoard= false;
-let countAttempt =0;
+let blockBoard = false;
+let countAttempt = 0;
 /*-------------------------------- Variables --------------------------------*/
 
 /*------------------------ Cached Element References ------------------------*/
 const cardElement = document.querySelectorAll('.memory-card');
 // console.log(cardElement.length)
-const attemptE= document.querySelector('.attempt');
-const MessageE= document.querySelector('.message');
+const attemptE = document.querySelector('.attempt');
+const MessageE = document.querySelector('.message');
+const resetButton = document.querySelector('.reset-game');
+const backFaceCard=document.querySelector('.back-face')
+// console.log(resetButton)
 
 /*----------------------------- Event Listeners -----------------------------*/
 for (let i = 0; i < cardElement.length; i++) {
     cardElement[i].addEventListener('click', flipCard);
 }
+resetButton.addEventListener('click', resetGame);
 
 /*-------------------------------- Functions --------------------------------*/
 
 //if we press it will add a class hide ehich will hide the back-side
 function flipCard(event) {
 
-    if(blockBoard)return;
+    if (blockBoard) return;
     event.target.classList.add('hide');
     const clickedCard = event.target;
+    // console.log(clickedCard)
     if (!firstCard) {
         // If no card is flipped, set the first card
         firstCard = clickedCard;
@@ -74,13 +79,15 @@ function checkMatch() {
         // Cards do not match
         console.log('unmatched')
         countAttempt++;
-        attemptE.textContent=`Attempt left ${countAttempt}/6`
+        attemptE.textContent = `Attempts left: ${countAttempt}/6`
 
-        if(countAttempt===6){
-            MessageE.textContent=`You lost`
+        if (countAttempt === 6) {
+            MessageE.textContent = `You lost`
+            resetButton.disabled = false;
+            blockBoard = true;
+            return
         }
-
-        console.log(countAttempt)
+        // console.log(countAttempt)
         unflipCards();
     }
 
@@ -96,7 +103,7 @@ function disableCards() {
 }
 
 function unflipCards() {
-    blockBoard=true;
+    blockBoard = true;
     //it delays execution
     setTimeout(function () {
         // Remove the 'hide' class to show the back side of the cards
@@ -104,7 +111,7 @@ function unflipCards() {
         secondCard.classList.remove('hide');
 
         resetBoard();
-        blockBoard=false;
+        blockBoard = false;
     }, 1000);
 }
 
@@ -113,11 +120,31 @@ function resetBoard() {
     secondCard = null;
 }
 
-function checkWin(){
-    const matchedCards=document.querySelectorAll('.memory-card .hide');
-    if(matchedCards.length===cardElement.length){
-            MessageE.textContent=`You Win`
+function checkWin() {
+    const matchedCards = document.querySelectorAll('.memory-card .hide');
+    if (matchedCards.length === cardElement.length) {
+        MessageE.textContent = `congratulations!!!!!, You Win. Your memory is good`
+        resetButton.disabled = false;
+        blockBoard = true;
+        return
+
     }
 }
 
+function resetGame() {
+    firstCard = null;
+    secondCard = null;
+    countAttempt = 0;
+    blockBoard = false;
+    resetButton.disabled = true;
+    attemptE.textContent = `Attempts left: ${countAttempt}/6`;
+    MessageE.textContent = `message`;
 
+    for (let i = 0; i < cardElement.length; i++) {
+        const frontFace = cardElement[i].querySelector('.front-face');
+        const backFace = cardElement[i].querySelector('.back-face');
+
+        if (frontFace) frontFace.classList.remove('hide');
+        if (backFace) backFace.classList.remove('hide');
+    }
+}
